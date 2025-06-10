1122baf956606eca12c4312a71620eb2276b49ff
Basna Yanti <basna.yanti.ft24@mail.umy.ac.id>
2025-06-10 11:12:29 +0700

    membuat method remove dan menggunakan if else untuk mengecek apakah antrian kosong

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -30,8 +30,26 @@ public:
         //Cek apakah antrian kosong
         if (FRONT == -1)
             REAR = 0;
-        else   
+        else 
+        {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
             REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
-    
+
+    void remove()
+{
+    //Cek apakah atrian kosong
+    if (FRONT == -1)
+    {
+        cout << "Queue underflow\n";
+        return;
+    }
+    cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+
+}
 };
